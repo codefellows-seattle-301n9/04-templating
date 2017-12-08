0dbb5f6f8dded74a8f3b2a1ad8b86cd6c909dd3a
Chris Lesesne <clesesne3@gmail.com>
2017-12-07 19:20:45 -0800

    comments, reviews, and TODOs complete; content renders correctly with handlebars

--- chris-ayanle/scripts/article.js
+++ chris-ayanle/scripts/article.js
@@ -3,23 +3,21 @@
 let articles = [];
 
 function Article (rawDataObj) {
-  this.author = rawDataObj.author;
-  this.authorUrl = rawDataObj.authorUrl;
-  this.title = rawDataObj.title;
-  this.category = rawDataObj.category;
-  this.body = rawDataObj.body;
-  this.publishedOn = rawDataObj.publishedOn;
+  for (let key in rawDataObj) {
+    this[key] = rawDataObj[key];
+  }
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let template = $('#articles-template').html();
+  let templateRender = Handlebars.compile(template);
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
@@ -30,12 +28,14 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  // DONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  return templateRender(this);
 };
 
-// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+
+// Parentheses are required around 'a' and 'b' because the .sort() method compares both/multiple values with each iteration. No parentheses ar required around the .forEach() method arguments because there is only one and arrow function is being used.
+
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
@@ -45,5 +45,5 @@ rawData.forEach(articleObject => {
 });
 
 articles.forEach(article => {
-  $('#articles').append(article.toHtml());
+  $('#blog').append(article.toHtml());
 });
