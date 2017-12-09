72673daf86a0c65e6d099e0ce6d18d184c2e2c0d
cody <codyjeromegreen@gmail.com>
2017-12-09 11:26:49 -0800

    working on the handlebars

--- cody-ixius/scripts/article.js
+++ cody-ixius/scripts/article.js
@@ -11,37 +11,38 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-console.log()
+
 Article.prototype.toHtml = function() {
 
-  var templateScript = $('#blog-template').html();
+  let templateScript = $('#blog-template').html();
 
-  var templateComplie = Handelbars.compile(templateScript);
-  
- articles.forEach(article => {
-   article.templateComplie();
- });
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
-
-  // REVIEW: If your template will use properties that aren't on the object yet, add them.
-  // Since your template can't hold any JS logic, we need to execute the logic here.
-  // The result is added to the object as a new property, which can then be referenced by key in the template.
-  // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
-
-  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
-
-  // REVIEW: The ternary operator above accomplishes this same logic.
-  // if(this.publishedOn) {
-  //   this.publishStatus = `published ${this.daysAgo} days ago`;
-  // } else {
-  //   this.publishStatus = '(draft)';
-  // }
-
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
-
+  let templateCompile = Handlebars.compile(templateScript);
+  return templateCompile(this);
+//  articles.forEach(article => {
+//    article.templateCompile();
+ 
 };
+// TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+
+// REVIEW: If your template will use properties that aren't on the object yet, add them.
+// Since your template can't hold any JS logic, we need to execute the logic here.
+// The result is added to the object as a new property, which can then be referenced by key in the template.
+// For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+
+
+this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+
+// REVIEW: The ternary operator above accomplishes this same logic.
+// if(this.publishedOn) {
+//   this.publishStatus = `published ${this.daysAgo} days ago`;
+// } else {
+//   this.publishStatus = '(draft)';
+// }
+
+// TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+
+
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
 // PUT YOUR RESPONSE HERE
