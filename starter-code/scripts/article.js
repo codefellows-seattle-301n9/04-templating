bccbaf5a0c9f3ec1aebf224a4cc547ed26c61a4d
Eric Cobb <eric.cobb2017@gmail.com>
2017-12-09 14:13:01 -0800

    forgot to save articles.js. Saved

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -13,11 +13,11 @@ function Article (rawDataObj) {
 
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  // TODONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
  let template = $('#blogArticles-template').html();
  console.log('this is a string', template);
  let templateRender = Handlebars.compile(template);
-  // REVIEW: If your template will use properties that aren't on the object yet, add them.
+  // REVIEWED: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
@@ -26,20 +26,20 @@ Article.prototype.toHtml = function() {
 
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
-  // REVIEW: The ternary operator above accomplishes this same logic.
+  // REVIEWED: The ternary operator above accomplishes this same logic.
   // if(this.publishedOn) {
   //   this.publishStatus = `published ${this.daysAgo} days ago`;
   // } else {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  // TODONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
   return templateRender(this);
   
 };
 
-// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+// PUT YOUR RESPONSE HERE: The difference is when there is no parenthesis is just calling the method but not the value of that method. Calling it with parenthesis, it is calling the value of that method. 
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
